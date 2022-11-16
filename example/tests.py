b470aaec374273b8781f24c200b30a1609036df9
nxiodev <dev@nxuni.io>
2022-11-15 19:28:50 -0600

    fast api service example

--- example/tests.py
+++ example/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class ProductosAPITest(TestCase):
+    """API para testear los servicios de productos"""
+
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_listar_productos(self):
+        url = reverse("productos")
+        respuesta = self.client.get(url)
+        self.assertEqual(respuesta.status_code, status.HTTP_200_OK)
